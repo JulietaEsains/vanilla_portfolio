994ebeaebe3ab686f394aeeeceb2a22ac5d6f9db
JulietaEsains <jesains@gmail.com>
2022-03-29 12:42:30 -0300

    Apartado de habilidades agregada

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,9 +22,25 @@ if (navCerrar) {
 /* ELIMINAR MENÚ PARA CELULAR */
 const navLink = document.querySelectorAll('.nav__link');
 
-function linkAction() {
+function irApartado() {
     const navMenu = document.getElementById('nav-menu');
     // Cuando hacemos click en cualquier nav__link, quitamos la clase "mostrar-menu"
     navMenu.classList.remove('mostrar-menu');
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', irApartado));
+
+/* HABILIDADES ACORDEÓN */
+const contenidoHabilidades = document.getElementsByClassName('contenido__habilidades'),
+      cabeceraHabilidades = document.querySelectorAll('.cabecera__habilidades');  
+
+function cambiarHabilidades() {
+    let claseItem = this.parentNode.className;
+
+    for(i = 0; i < contenidoHabilidades.length; i++) {
+        contenidoHabilidades[i].className = 'contenido__habilidades habilidades__cerrado';
+    }
+    if(claseItem === 'contenido__habilidades habilidades__cerrado') {
+        this.parentNode.className = 'contenido__habilidades habilidades__abierto';
+    }
+}
+cabeceraHabilidades.forEach((el) => el.addEventListener('click', cambiarHabilidades));
